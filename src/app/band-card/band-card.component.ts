bf1bf5613df271cdd0991ff3b2c1ac143b79a001
Vladlan <lanvlad911@gmail.com>
2018-05-15 20:04:09 +0300

    removed redundand js code from component

--- src/app/band-card/band-card.component.ts
+++ src/app/band-card/band-card.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, OnInit, Input, Output } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "app-band-card",
-  templateUrl: "./band-card.component.html",
-  styleUrls: ["./band-card.component.css"]
+  selector: 'app-band-card',
+  templateUrl: './band-card.component.html',
+  styleUrls: ['./band-card.component.css']
 })
 export class BandCardComponent {
   @Input() title: string;
@@ -11,12 +11,4 @@ export class BandCardComponent {
   @Input() description: string;
   @Input() genre: string;
   @Input() bandUrl: string;
-
-  onEnter(el: any) {
-    el.target.childNodes[0].childNodes[1].style.display = "block";
-  }
-
-  onLeave(el: any) {
-    el.target.childNodes[0].childNodes[1].style.display = "none";
-  }
 }
