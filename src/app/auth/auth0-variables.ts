62db1b3330331445d991429f192ba6ca1b280197
Vladlan <lanvlad911@gmail.com>
2018-05-21 18:51:55 +0300

    added another auth0 ClientId

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -5,7 +5,7 @@ interface AuthConfig {
 }
 
 export const AUTH_CONFIG: AuthConfig = {
-  clientID: 'yMOoMB10FCDcX2Pqa33PXeXNsSrNcZ7a',
+  clientID: 'FkZcNR9scfSVpedeEhlOsCjFdOQU75n2',
   domain: 'band-camp-clone.eu.auth0.com',
   callbackURL: 'http://178.172.173.195:8080/callback'
 };
