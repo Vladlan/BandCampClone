86dc6dc5aac683429e0d3b2c01f9abd1f70080dc
Vladlan <lanvlad911@gmail.com>
2018-05-21 17:57:26 +0300

    Update auth0-variables.ts

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'yMOoMB10FCDcX2Pqa33PXeXNsSrNcZ7a',
   domain: 'band-camp-clone.eu.auth0.com',
-  callbackURL: 'http://178.172.173.195:8010/callback'
+  callbackURL: 'http://178.172.173.195:8080/callback'
 };
