42a6afd014b11d03d96833de6fdb577fcbdcfdbd
Anton Postoyalko <ufon.exe@yandex.ru>
2018-05-28 18:51:55 +0300

    admin-bar-component refactor logic for new localstorage service.

--- src/app/admin/admin-bar/admin-bar.component.ts
+++ src/app/admin/admin-bar/admin-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { LocalStorageService } from 'app/services/localstorage.service';
+import { Component, OnInit, COMPILER_OPTIONS } from '@angular/core';
+import { LocalStorageService } from 'app/services';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs/Subject';
 
 interface AdminValue {
+  key: string;
   value: any;
 }
 
@@ -13,54 +15,59 @@ interface AdminValue {
 })
 export class AdminBarComponent implements OnInit {
   open: boolean;
-  callToAction: AdminValue;
+
+  callToAction: boolean;
+
   adminBg: AdminValue;
-  lightTheme: AdminValue;
+  adminTheme: AdminValue;
 
-  formAdminBg: FormGroup;
-  formCallToAction: FormGroup;
-  formAdminTheme: FormGroup;
+  adminBgColors: Array<AdminValue> = [
+    { value: 'Default', key: '#9dc3ce' },
+    { value: 'Red', key: 'red' },
+    { value: 'Green', key: 'green' }
+  ];
 
-  constructor(private globalService: LocalStorageService) {}
+  adminThemes: Array<AdminValue> = [
+    { value: 'Light', key: 'white' },
+    { value: 'Black', key: 'black' }
+  ];
+
+  constructor(protected localStorage: LocalStorageService) {}
 
   ngOnInit() {
-    this.globalService.adminBg === null
-      ? (this.adminBg = { value: '#9dc3ce' })
-      : (this.adminBg = this.globalService.adminBg);
-    this.globalService.adminTheme === null
-      ? (this.lightTheme = { value: false })
-      : (this.lightTheme = this.globalService.adminTheme);
-    this.globalService.callToAction === null
-      ? (this.callToAction = { value: false })
-      : (this.callToAction = this.globalService.callToAction);
-
-    this.formAdminBg = new FormGroup({
-      adminBg: new FormControl(this.adminBg.value)
-    });
-    this.formAdminBg.valueChanges.subscribe(term => {
-      this.adminBg.value = term.adminBg;
-      this.globalService.adminBg = this.adminBg;
-    });
-
-    this.formCallToAction = new FormGroup({
-      callToAction: new FormControl(this.callToAction.value)
-    });
-    this.formCallToAction.valueChanges.subscribe(term => {
-      this.callToAction = term.callToAction;
-      this.globalService.callToAction = this.callToAction;
-    });
-
-    this.formAdminTheme = new FormGroup({
-      adminTheme: new FormControl(this.lightTheme.value)
-    });
-
-    this.formAdminTheme.valueChanges.subscribe(term => {
-      this.lightTheme.value = term.adminTheme;
-      this.globalService.adminTheme = this.lightTheme;
-    });
+    this.localStorage.getItem('adminBg').subscribe(data => {
+      this.adminBg = data;
+      console.log(data);
+    }, () => (this.adminBg = this.adminBgColors[0]));
+    this.localStorage.getItem('adminTheme').subscribe(data => {
+      this.adminTheme = data;
+      console.log(data);
+    }, () => (this.adminTheme = this.adminThemes[0]));
+    this.localStorage.getItem('callToAction').subscribe(data => {
+      this.callToAction = data;
+      console.log(data);
+    }, () => (this.callToAction = true));
   }
 
-  toogleSide() {
-    this.open = !this.open;
-  }
+  setAdminBg = (key: string): void => {
+    const adminBg = this.adminBgColors.find(color => color.key === key);
+    this.localStorage
+      .setItem('adminBg', adminBg)
+      .subscribe(() => (this.adminBg = adminBg));
+  };
+
+  setAdminTheme = (key: string): void => {
+    const adminTheme = this.adminThemes.find(color => color.key === key);
+    this.localStorage
+      .setItem('adminTheme', adminTheme)
+      .subscribe(() => (this.adminTheme = adminTheme));
+  };
+
+  toogleCallToAction = (key: string): void => {
+    this.localStorage
+      .setItem('callToAction', !!key)
+      .subscribe(() => (this.callToAction = !!key));
+  };
+
+  toogleSide = () => (this.open = !this.open);
 }
