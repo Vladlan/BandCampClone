a54754397e57d311179b7429153f84dcd42870f1
Anton Postoyalko <ufon.exe@yandex.ru>
2018-05-28 18:49:44 +0300

    Refactor BandNameGenreFilterPipe.

--- src/app/pipes/bands-filter.pipe.ts
+++ src/app/pipes/bands-filter.pipe.ts
@@ -1,24 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { BandsListItemComponent } from '../band/bands-list-item/bands-list-item.component';
-
+import { Band } from 'app/models';
 @Pipe({
   name: 'BandNameGenreFilterPipe'
 })
 export class BandNameGenreFilterPipe implements PipeTransform {
-  transform(bands, searchStr: string) {
-    if (bands.length === 0 || searchStr === undefined) {
-      return bands;
-    }
-
-    return bands.filter(band => {
-      if (
-        band['title'].toLowerCase().indexOf(searchStr.toLowerCase()) !== -1 ||
-        band['genre'].toLowerCase().indexOf(searchStr.toLowerCase()) !== -1
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-  }
+  transform = (bands: Array<Band>, searchStr: string) =>
+    bands.filter(
+      band =>
+        band['title'].toLowerCase().includes(searchStr.toLowerCase()) ||
+        band['genre'].toLowerCase().includes(searchStr.toLowerCase())
+    );
 }
