072370343c49efc4175cfc9f4a4045630d48a095
Anton Postoyalko <ufon.exe@yandex.ru>
2018-06-05 10:24:34 +0300

    add working admin-panel

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CallbackComponent } from './callback/callback.component';
 
-const routes: Routes = [{ path: 'callback', component: CallbackComponent }];
+const routes: Routes = [{ path: '', component: CallbackComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
